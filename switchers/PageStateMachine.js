2f900accc990351db84ef4c35cd2bc3f9544a132
Hadi Chahine <hadichahine232@hotmail.com>
2019-03-21 14:45:28 +0200

    Some method extractings and renamings for state machine logic.

--- switchers/PageStateMachine.js
+++ switchers/PageStateMachine.js
@@ -8,16 +8,28 @@ module.exports = class PageStateMachine{
 
     next(){
         if(this.hasMoreThanOnePage() && this.isNotOnFinalPage()){
-            this.stateMachineTransitionHandler.doNext(this.pages[this.currentPageIndex],this.pages[this.currentPageIndex + 1]);
+            this.stateMachineTransitionHandler.doNext(this.currentPage(),this.nextPage());
             this.currentPageIndex++;
         }
     }
 
+    nextPage() {
+        return this.pages[this.currentPageIndex + 1];
+    }
+
+    currentPage() {
+        return this.pages[this.currentPageIndex];
+    }
+
     backwards(){
         if(this.hasMoreThanOnePage() && this.isNotOnFirstPage()){
-            this.stateMachineTransitionHandler.doBackwards(this.pages[this.currentPageIndex],this.pages[this.currentPageIndex - 1]);
+            this.stateMachineTransitionHandler.doBackwards(this.currentPage(),this.previousPage());
             this.currentPageIndex--;
         }
+    }
+
+    previousPage() {
+        return this.pages[this.currentPageIndex - 1];
     }
 
     isNotOnFirstPage() {
